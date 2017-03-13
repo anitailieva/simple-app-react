12099f97207311885b489cf85d54f46e8b4d5ae8
Anita Ilieva <anita.ilieva24@gmail.com>
2017-03-13 22:23:48 +0100

    Validating with propTypes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
+import './stylesheets/ui.scss'
 import { SkiDayList } from './components/SkiDayList'
 
 window.React = React
 
 render(
-	<SkiDayList days={
+    <SkiDayList days={
         [
             {
                 resort: "Squaw Valley",
